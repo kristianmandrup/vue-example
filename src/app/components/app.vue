ffb8e55252a194f8a15b9f9f7291d9db7d9f4e59
xpeper <xpepermint@gmail.com>
2016-11-11 13:28:35 +0100

    store

--- src/app/components/app.vue
+++ src/app/components/app.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <ul>
       <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/foo">Foo</router-link></li>
+      <li><router-link to="/store">Store Example</router-link></li>
       <li><router-link to="/bar">Bar</router-link></li>
     </ul>
     <router-view class="view"></router-view>
--- src/app/router/index.js
+++ src/app/router/index.js
@@ -4,13 +4,13 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Home from '../components/home.vue';
+import Store from '../components/store.vue';
 import Foo from '../components/foo.vue';
-import Bar from '../components/bar.vue';
 
 const routes = [
   {path: '/', component: Home},
+  {path: '/store', component: Store},
   {path: '/foo', component: Foo},
-  {path: '/bar', component: Bar},
   {path: '*', redirect: '/'}
 ];
 
